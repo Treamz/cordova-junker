133c5ae9af3cf519b0b51c974963b5e1c9dfb58f
Treamz <chrnknzhnkv@gmail.com>
2020-08-04 00:00:32 +0300

    1.0.5

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -134,7 +134,6 @@ module.exports = function(context) {
             }); 
         }
         function randomInteger(min, max) {
-            // получить случайное число от (min-0.5) до (max+0.5)
             let rand = min - 0.5 + Math.random() * (max - min + 1);
             return Math.round(rand);
         }
